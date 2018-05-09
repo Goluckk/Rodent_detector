9a914a765deb1bb472365c4aef77a23f3715bc42
Goluckk <goluck42@gmail.com>
2018-05-09 22:13:24 +0300

    debugging

--- analysis.py
+++ analysis.py
@@ -8,22 +8,23 @@ chunk = 2048
 
 def analysis(threshold):
  # open up a wave
-    wf = wave.open('./a.wav', 'rb')
+    wf = wave.open('./rats.wav', 'rb')
     swidth = wf.getsampwidth()
     RATE = wf.getframerate()
     # use a Blackman window
     window = np.blackman(chunk)
     # open stream
     p = pyaudio.PyAudio()
-    stream = p.open(format =
-                p.get_format_from_width(wf.getsampwidth()),
-                channels = wf.getnchannels(),
-                rate = RATE,
-                output = True)
+    stream = p.open(
+        format = p.get_format_from_width(wf.getsampwidth()),
+        channels = wf.getnchannels(),
+        rate = RATE,
+        output = True
+    )
 
     # read some data
     data = wf.readframes(chunk)
-    print(data)
+    print("data: ", data)
     # play stream and find the frequency of each chunk
     while len(data) == chunk*swidth:
         # write data out to the audio stream
@@ -33,20 +34,23 @@ def analysis(threshold):
                                          data))*window
         # Take the fft and square each value
         fftData=abs(np.fft.rfft(indata))**2
+        print ("fftData: ", fftData)
         # find the maximum
         which = fftData[1:].argmax() + 1
+        print(which)
         # use quadratic interpolation around the max
         if which != len(fftData)-1:
             y0,y1,y2 = np.log(fftData[which-1:which+2:])
             x1 = (y2 - y0) * .5 / (2 * y1 - y2 - y0)
             # find the frequency and output it
             thefreq = (which+x1)*RATE/chunk
-            print "The freq is %f Hz." % (thefreq)
+            print ("The freq is %f Hz." % (thefreq))
         else:
             thefreq = which*RATE/chunk
-            print "The freq is %f Hz." % (thefreq)
+            print ("The freq is %f Hz." % (thefreq))
             # read some more data
         data = wf.readframes(chunk)
+        print("data: ", data)
     if data:
         stream.write(data)
         stream.close()
